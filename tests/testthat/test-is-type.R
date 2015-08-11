034c40d844207f39eb16558fce2c75a63fbebd5e
Richard Cotton <rjc2003@qatar-med.cornell.edu>
2015-08-11 16:46:23 +0300

    updated tests

--- tests/testthat/test-is-type.R
+++ tests/testthat/test-is-type.R
@@ -135,26 +135,6 @@ test_that("test.is_language.not_a_language_object.returns_false", {
   expect_false(is_language(sin))
 })
 
-test_that("test.is_leaf.a_false_leaf.returns_false", {
-  x <- structure(list(), leaf = FALSE)
-  expect_false(is_leaf(x))
-})
-
-test_that("test.is_leaf.a_leaf.returns_true", {
-  x <- structure(list(), leaf = TRUE)
-  expect_true(is_leaf(x))
-})
-
-test_that("test.is_leaf.a_non_logical_leaf.returns_false", {
-  x <- structure(list(), leaf = 1:10)
-  expect_false(is_leaf(x))
-})
-
-test_that("test.is_leaf.a_null_leaf.returns_false", {
-  x <- list()
-  expect_false(is_leaf(x))
-})
-
 test_that("test.is_list.a_list.returns_true", {
   expect_true(is_list(list(1, 2, 3)))
 })
@@ -257,19 +237,6 @@ test_that("test.is_s4.not_an_S4_instance.returns_true", {
   expect_false(is_s4(1:10))
 })
 
-test_that("test.is_stepfun.a_regular_function.returns_false", {
-  expect_false(is_stepfun(function() {}))
-})
-
-test_that("test.is_stepfun.a_step_function.returns_true", {
-  x <- stepfun(1:3, c(1, 2, 4, 3), f = 0)
-  expect_true(is_stepfun(x))
-})
-
-test_that("test.is_stepfun.not_a_function.returns_false", {
-  expect_false(is_stepfun(call("sin", "pi")))
-})
-
 test_that("test.is_table.a_table.returns_true", {
   x <- table(sample(letters, 100, replace = TRUE))
   expect_true(is_table(x))
@@ -277,22 +244,6 @@ test_that("test.is_table.a_table.returns_true", {
 
 test_that("test.is_table.not_a_table.returns_false", {
   expect_false(is_table(1:10))
-})
-
-test_that("test.is_ts.a_time_series.returns_true", {
-  expect_true(is_ts(ts(1:10)))
-})
-
-test_that("test.is_ts.not_a_time_series.returns_false", {
-  expect_false(is_ts(1:10))
-})
-
-test_that("test.is_tskernel.a_time_series_kernel.returns_true", {
-  expect_true(is_tskernel(kernel("daniell", 10)))
-})
-
-test_that("test.is_tskernel.not_a_time_series_kernel.returns_false", {
-  expect_false(is_tskernel(1:10))
 })
 
 test_that("test.is2.1_to_5_is_list.returns_false", {
